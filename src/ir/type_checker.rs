e8d76aed7686294370efd79d960bc4aa51af6b41
John DiSanti <johndisanti@gmail.com>
2017-12-31 21:44:46 -0800

    Add open source licenses

--- src/ir/type_checker.rs
+++ src/ir/type_checker.rs
@@ -1,3 +1,12 @@
+//
+// Copyright 2017 hasselc Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+
 use error::{self, ErrorKind};
 use ir::block::{Block, CallData, Expr, Statement};
 use src_tag::SrcTagged;
